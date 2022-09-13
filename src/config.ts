f759d7b69588837e6bdeb309b42bf9ff2d4e3d9e
ykw <1794093413@qq.com>
2022-09-14 01:25:54 +0800

    ci:  db-migrate init

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
-export const isDev = process.env.NODE_ENV === 'development';
+export const isDev = process.env.NODE_ENV === 'dev';
 
 export const DB_HOST = process.env.DB_HOST;
 export const DB_PORT = process.env.DB_PORT;
